a1c388aa88db12e9cedd7bf55c5021f752a2ef44
Pavel <54819066+Fischerski1@users.noreply.github.com>
2020-09-21 10:05:36 +0300

    + class SocketProcessor

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,14 +1,35 @@
-import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-class HttpServer extends Thread {
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
+public class HttpServer {
+
+    public static void main(String[] args) throws Throwable {
+        ServerSocket ss = new ServerSocket(8080);
         while (true) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Client accepted");
+            Socket socket = ss.accept();
+            System.err.println("Client accepted");
+            new Thread(new SocketProcessor(socket)).start();
+        }
+    }
+
+
+    private static class SocketProcessor implements Runnable {
+        private Socket socket;
+        private InputStream is;
+        private OutputStream os;
+
+        public SocketProcessor(Socket socket) {
+            this.socket = socket;
+            this.is = is;
+            this.os = os;
+        }
+
+        @Override
+        public void run() {
+
         }
     }
 }
